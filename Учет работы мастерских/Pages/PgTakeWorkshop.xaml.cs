fad994e4914c0630cbfb0872c7ba0956f8964b07
alexkolesov2002 <alladin.kolesov@mail.ru>
2022-05-04 12:52:42 +0300

    1

--- Учет работы мастерских/Pages/PgTakeWorkshop.xaml.cs
+++ Учет работы мастерских/Pages/PgTakeWorkshop.xaml.cs
@@ -24,9 +24,13 @@ namespace Учет_работы_мастерских
         public event PropertyChangedEventHandler PropertyChanged;
         List<journal_use_workshop> journal_Use_Workshop = new List<journal_use_workshop>();
         List<Criterion> CriterionTextList = new List<Criterion>();
+        List<criteria> CriterionList = new List<criteria>();
+        List<criteria> CriterionListBuf = new List<criteria>();
         List<students> students = BaseModel.BaseConnect.students.ToList();
         List<students> studentsBufForAdd = new List<students>();
+        List<students> studentsBufForAdd2 = BaseModel.BaseConnect.students.ToList();
         List<students> studentsBufForResiult = new List<students>();
+        List<criteria_in_event> criteria_In_Events = new List<criteria_in_event>();
 
 
 
@@ -44,7 +48,7 @@ namespace Учет_работы_мастерских
                 ComboBoxCompetisionSelect.DisplayMemberPath = "title_type_event";
                 ComboBoxCompetisionSelect.SelectedValuePath = "id_type_event";
                 studentsBufForAdd = students;
-               
+
                 //ListPickedStudent.ItemsSource = studentsBufForResiult;
 
             }
@@ -66,18 +70,28 @@ namespace Учет_работы_мастерских
                     await Task.Delay(4);
                     PropertyChanged(this, new PropertyChangedEventArgs("Loading"));
                 }
+                foreach (Criterion criteria in (List<Criterion>)ListCriterion.ItemsSource)
+                {
+                    criteria criterion = new criteria() { title_criterion = criteria.title_criterion };
+                    CriterionList.Add(criterion);
+                }
+                BaseModel.BaseConnect.criteria.AddRange(CriterionList);
 
                 events events = new events() { exercise = TxtExercise.Text, title_event = TxtExercise.Text, id_type_event = (int)ComboBoxCompetisionSelect.SelectedValue, topic_of_the_lesson = TxtTopic.Text };
                 BaseModel.BaseConnect.events.Add(events);
                 BaseModel.BaseConnect.SaveChanges();
                 events NewEvent = BaseModel.BaseConnect.events.SingleOrDefault(x => x.exercise == TxtExercise.Text && x.topic_of_the_lesson == TxtTopic.Text && x.id_type_event == (int)ComboBoxCompetisionSelect.SelectedValue);
+                
+                 await Task.Delay(15);
 
-                await Task.Delay(15);
-                SaveNotComplete = false;
-                PropertyChanged(this, new PropertyChangedEventArgs("SaveNotComplete"));
-                IsSaveComplete = true;
-                PropertyChanged(this, new PropertyChangedEventArgs("IsSaveComplete"));
-
+                foreach (students students in (List<students>)ListPickedStudent.ItemsSource)
+                {
+                    foreach (criteria criteria in CriterionList)
+                    {
+                        criteria_in_event criterion = new criteria_in_event() { id_criterion = criteria.id_criterion, id_event = NewEvent.id_event, };
+                        criteria_In_Events.Add(criterion);
+                    }
+                }
                 string Date = CalendarDate.SelectedDate.Value.ToShortDateString();
                 string Time = ClockTime.Time.ToLongTimeString();
 
@@ -99,7 +113,11 @@ namespace Учет_работы_мастерских
                 }
 
                 BaseModel.BaseConnect.SaveChanges();
-                //BtnSaveData.IsEnabled = false;
+                BtnSaveData.IsEnabled = false;
+                SaveNotComplete = false;
+                PropertyChanged(this, new PropertyChangedEventArgs("SaveNotComplete"));
+                IsSaveComplete = true;
+                PropertyChanged(this, new PropertyChangedEventArgs("IsSaveComplete"));
                 //LoadPages.SwitchPages.Navigate(new PgTakeEquip((workshops)ComboBoxWorkShops.SelectedItem));
             }
             catch (Exception ex)
@@ -153,19 +171,24 @@ namespace Учет_работы_мастерских
 
         private void BtnAddStudent_Click(object sender, RoutedEventArgs e)
         {
-            Button button =(Button)sender;
+            Button button = (Button)sender;
             int id = Convert.ToInt32(button.Uid);
             studentsBufForResiult.Add(studentsBufForAdd.FirstOrDefault(x => x.id_student == id));
+            studentsBufForAdd2.Remove(studentsBufForAdd.FirstOrDefault(x => x.id_student == id));
             ListPickedStudent.ItemsSource = studentsBufForResiult;
+            ListAddStudent.ItemsSource = studentsBufForAdd2;
+            ListAddStudent.Items.Refresh();
             ListPickedStudent.Items.Refresh();
+            FindStudent();
         }
 
         async void FindStudent()
         {
             studentsBufForAdd = students;
+
             if (TxtFindStudent.Text != "")
             {
-                studentsBufForAdd = studentsBufForAdd.FindAll(x => x.FullName.Contains(TxtFindStudent.Text)).ToList();
+                studentsBufForAdd = studentsBufForAdd2.FindAll(x => x.FullName.Contains(TxtFindStudent.Text)).ToList();
                 ListAddStudent.ItemsSource = studentsBufForAdd;
                 ListAddStudent.Items.Refresh();
                 ListAddStudent.Visibility = Visibility.Visible;
@@ -188,9 +211,13 @@ namespace Учет_работы_мастерских
         {
             Button button = (Button)sender;
             int id = Convert.ToInt32(button.Uid);
-            studentsBufForResiult.Remove(studentsBufForAdd.FirstOrDefault(x => x.id_student == id));
+            studentsBufForAdd2.Add(studentsBufForResiult.FirstOrDefault(x => x.id_student == id));
+            studentsBufForResiult.Remove(studentsBufForResiult.FirstOrDefault(x => x.id_student == id));
+            ListAddStudent.ItemsSource = studentsBufForAdd2;
             ListPickedStudent.ItemsSource = studentsBufForResiult;
             ListPickedStudent.Items.Refresh();
+            ListAddStudent.Items.Refresh();
+            FindStudent();
         }
     }
 }
