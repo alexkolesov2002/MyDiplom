723c89b814bed1c5c6a3ee6bfabdef670c288044
alexkolesov2002 <alladin.kolesov@mail.ru>
2022-05-27 13:29:51 +0300

    1

--- Учет работы мастерских/PartialClasses/PartCriteria_In_Event.cs
+++ Учет работы мастерских/PartialClasses/PartCriteria_In_Event.cs
@@ -1,9 +1,44 @@
-﻿namespace Учет_работы_мастерских
+﻿using System;
+using System.Windows;
+
+namespace Учет_работы_мастерских
 {
     partial class criteria_in_event
 
     {
-        //public List<criteria> listCriterial { get; set; } = new List<criteria>();
-        //public List<students> listStudent { get; set; } = new List<students>();
+
+
+        public int Rating
+        {
+            get
+            {
+                if (rating == null) return 0;
+                else
+                {
+                    return
+                        (int)rating;
+                }
+            }
+            set
+            {
+                try
+                {
+                    if (value > 101 || value < 0)
+                    {
+                        throw new Exception("Некорректо введены данные");
+                    }
+                    else
+                    {
+                        rating = value;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
+
     }
 }
